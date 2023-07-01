39cf15ac35fc6a42cb6db551550a7bdbcb8668eb
Giulia s <g.schillaci@live.it>
2023-07-01 22:52:17 +0200

    aggiunta lista contatti

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+require_once $_SERVER["DOCUMENT_ROOT"] . '\vendor\autoload.php';
 
+use User\DatabaseAbstraction\DatabaseFactory;
+use User\DatabaseAbstraction\DatabaseContract;
+
+$database = DatabaseFactory::Create(DatabaseContract::TYPE_PDO);
 ?>
 
 <!DOCTYPE html>
@@ -17,11 +22,24 @@
 </head>
 
 <body>
-    <a href="./pages/formAggiungiContatto.php">
-        <button class="btn btn-primary">
-            aggiungi contatto
-        </button>
-    </a>
+    <div class="container justify-content-center">
+        <ul class="list-group">
+            <?php $result = $database->getData("SELECT * FROM contacts"); ?>
+            <?php while ($contact = $result->fetch()): ?>
+            <li class="list-group-item">
+                <?= $contact["nome"] ?>
+                <?= $contact["cognome"] ?> -
+                <?= $contact["numero"] ?> -
+                <?= $contact["email"] ?>
+            </li>
+            <?php endwhile; ?>
+        </ul>
+        <a href="./pages/formAggiungiContatto.php">
+            <button class="btn btn-primary">
+                aggiungi contatto
+            </button>
+        </a>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
